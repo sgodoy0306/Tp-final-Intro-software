0d6bca2130d57cf9b7ea9ac351aff989db169e68
Guti <agutierreza@fi.uba.ar>
2025-07-14 21:22:28 +0000

    Agrego cambios para poder deployar

--- backend/src/scripts/catalogo.js
+++ backend/src/scripts/catalogo.js
@@ -1,11 +1,7 @@
 const { Pool } = require("pg");
 
 const dbClient = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  connectionString: process.env.DATABASE_URL,
 });
 
 // Juegos
