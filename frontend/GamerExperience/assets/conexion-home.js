883b239fa9081fc919b3f8bd3473f1545281e270
asus_fausto <faustogiannottiur@gmail.com>
2025-07-16 22:06:33 -0300

    cambios para que pueda filtar por consola

--- frontend/GamerExperience/assets/conexion-home.js
+++ frontend/GamerExperience/assets/conexion-home.js
@@ -4,14 +4,14 @@ document.addEventListener("DOMContentLoaded", async () => {
   if (!platformsContainer) return;
 
   try {
-    const response = await fetch("https://tp-final-intro-software.onrender.com/api/consolas");
+    const response = await fetch("http://localhost:8000/api/consolas");
     const consolas = await response.json();
 
     consolas.forEach((consola) => {
       const a = document.createElement("a");
-      a.href = `./juegos.html?consola=${consola.id}`;
+      a.href = `./juegos?consola=${consola.id}`;
       a.className =
-        "flex-1 min-w-36 max-w-xs w-full h-24 sm:h-32 bg-black rounded-xl flex items-center justify-center shadow-[0_4px_0_0_#444,0_8px_0_0_#000] border-2 border-gray-400 hover:border-green-400 active:shadow-none active:translate-y-1 transition-all duration-150 cursor-pointer select-none relative overflow-hidden opacity-0 -translate-x-16 delay-0 catalog-item hover:flex-2 hover:delay-0 hover:duration-200";
+        "flex-1 min-w-36 max-w-xs w-full h-24 sm:h-32 bg-black rounded-xl flex items-center justify-center shadow-[0_4px_0_0_#444,0_8px_0_0_#000] border-2 border-gray-400 hover:border-green-400 active:shadow-none active:translate-y-1 transition-all duration-150 cursor-pointer select-none relative overflow-hidden opacity-100 translate-x-0 delay-0 catalog-item hover:flex-2 hover:delay-0 hover:duration-200";
       a.innerHTML = `
                 <img src="${consola.url_imagen || consola.URL_IMAGEN}" alt="${
         consola.nombre || consola.Nombre
